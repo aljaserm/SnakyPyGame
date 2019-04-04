3dccee8ed34043037f664d4849fe2f0cb0b7f5c4
Mohammed alJaser <aljaserm7@gmail.com>
2019-04-03 20:00:50 -0400

    Completed the project

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 # THIS SCRIPT SHOULD BE CALLED SETUP.PY
 import cx_Freeze
 import os
+
 executables = [
-        #                   name of your game script
-        cx_Freeze.Executable("snake.py")
+    #                   name of your game script
+    cx_Freeze.Executable("snake.py")
 ]
 
-os.environ['TCL_LIBRARY']=r'C:\Users\aljas\AppData\Local\Programs\Python\Python37\tcl\tcl8.6'
-os.environ['TK_LIBRARY']=r'C:\Users\aljas\AppData\Local\Programs\Python\Python37\tcl\tcl8.6'
+os.environ['TCL_LIBRARY'] = r'C:\Users\aljas\AppData\Local\Programs\Python\Python37\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\aljas\AppData\Local\Programs\Python\Python37\tcl\tcl8.6'
 
 cx_Freeze.setup(
-        name = "ClassicSnake",
-        options = {"build_exe": {"packages":["pygame"],"include_files":[]}},
-        description = "",
-        executables = executables)
+    name="ClassicSnake",
+    options={"build_exe": {"packages": ["pygame"], "include_files": []}},
+    description="",
+    executables=executables)
 
-
-#python setup.py build
-#python setup.py bdist_msi
+# python setup.py build
+# python setup.py bdist_msi
